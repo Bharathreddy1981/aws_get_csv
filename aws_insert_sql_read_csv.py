6b959c293e65338c8da69ac54ef81f2f84bb3a87
Bharathreddy1981 <70434245+Bharathreddy1981@users.noreply.github.com>
2020-10-28 08:32:00 +0530

    getting the csv file for particular id

---
+++ aws_insert_sql_read_csv.py
@@ -0,0 +1,46 @@
+
+#read the given query of mysql in csv or excel
+
+from flask import Flask,jsonify,request
+#pip install openpyxl module
+import pymysql
+import pandas as pd
+
+
+aws_insert_sql_read_csv = Flask(__name__)
+@aws_insert_sql_read_csv.route("/csv", methods=["POST"])
+def read():
+
+    jsondata=request.get_json()
+    image=jsondata["image"]
+    #value=jsondata["image"].split('/')[-1]
+    #print(value)
+
+    z = pymysql.connect(
+                host="database-1.cfmudpl0e2d7.ap-south-1.rds.amazonaws.com",
+                user="admin",
+                passwd="bharath**",
+                db="transction_prod"
+    )
+    #y = z.cursor()
+
+    df=pd.read_sql_query("select * from student where studentname='" + str(image) + "'",con=z)
+    df.to_csv("anil.csv",index=False)
+
+    print(df)
+    #query = "select * from student where id='" + str(number) + "'"
+
+    #y.execute(query)
+    #bha = y.fetchall()
+
+
+    #for i in bha:
+    #    k={"id":i[0],"studentname":i[1]," phone":i[2],"email":i[3],"city":i[4]}
+
+    return
+
+if __name__=="__main__":
+    aws_insert_sql_read_csv.run(host='0.0.0.0')
+
+
+
